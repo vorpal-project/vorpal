6e67d2952b1974f7f17182240daf0c76e8ce9100
Kazuo256 <kazuo256@gmail.com>
2015-04-29 18:48:37 -0300

    Reference for System class

--- src/oda/system.cxx
+++ src/oda/system.cxx
@@ -13,11 +13,14 @@ ALCcontext  *context = nullptr;
 System::System() {}
 
 Status System::start() {
+  // Do not start if the context was already created
   if (context && device)
     return Status::INVALID("Already started");
+  // Open device
   device = alcOpenDevice(nullptr);
   if (!device)
     return Status::FAILURE("Could not open a device");
+  // Create and set context
   context = alcCreateContext(device, nullptr);
   if (!context || alcMakeContextCurrent(context) == ALC_FALSE) {
     if (context)
@@ -25,13 +28,17 @@ Status System::start() {
     alcCloseDevice(device);
     return Status::FAILURE("Coult not set a context");
   }
+  // Tell which device was opened
   return Status::OK(alcGetString(device, ALC_DEVICE_SPECIFIER));
 }
 
 void System::finish() {
+  // Do not finish if it was not started yet
   if (!context) return;
+  // Unset and destroy context
   alcMakeContextCurrent(nullptr);
   alcDestroyContext(context);
+  // Close device
   alcCloseDevice(device);
 }
 
--- src/oda/system.h
+++ src/oda/system.h
@@ -6,13 +6,33 @@
 
 namespace oda {
 
-/// First Open Dynamic Audio main system class.
+/// Open Dynamic Audio system management class.
+/** 
+ * All instances of this class handle the same shared resources. Thus, there
+ * is no need to dynamically allocate objects for it.
+ * 
+ * Use the `start()` method to initialize the system, and the `finish()` method
+ * to release it. Example:
+ *
+ * ~~~.cxx
+ * oda::System sys;
+ * oda::Status status = sys.start();
+ * // check return value
+ * // use the system
+ * sys.finish();
+ * ~~~
+ * @see start()
+ * @see finish()
+ */
 class System {
  public:
   System();
   ~System() {}
 
+  /// Starts the main ODA system
   Status start();
+
+  /// Finishes the main ODA system
   void finish();
 };
  
