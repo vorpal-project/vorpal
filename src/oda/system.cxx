4f21605d1ca43887b828870003d0df92da078709
Kazuo256 <kazuo256@gmail.com>
2015-04-30 15:08:58 -0300

    Libpd is installed together with liboda + libpd init

--- src/oda/system.cxx
+++ src/oda/system.cxx
@@ -1,6 +1,8 @@
 
 #include <oda/system.h>
 
+#include <libpd/z_libpd.h>
+
 #include <AL/al.h>
 #include <AL/alc.h>
 #include <AL/alext.h>
@@ -28,6 +30,10 @@ Status System::start() {
     alcCloseDevice(device);
     return Status::FAILURE("Coult not set a context");
   }
+  // Start libpd
+  libpd_init();
+  if (libpd_init_audio(1, 1, 44100) != 0)
+    return Status::FAILURE("Could not initialize DSP");
   // Tell which device was opened
   return Status::OK(alcGetString(device, ALC_DEVICE_SPECIFIER));
 }
@@ -38,8 +44,10 @@ void System::finish() {
   // Unset and destroy context
   alcMakeContextCurrent(nullptr);
   alcDestroyContext(context);
+  context = nullptr;
   // Close device
   alcCloseDevice(device);
+  device = nullptr;
 }
 
 } // namespace oda
