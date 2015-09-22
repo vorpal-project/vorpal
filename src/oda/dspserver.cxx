f61051a968836a11e4eeeae71b28e552f800dd58
Kazuo256 <kazuo256@gmail.com>
2015-09-22 12:42:04 -0300

    ODA Panel take one!

--- src/oda/dspserver.cxx
+++ src/oda/dspserver.cxx
@@ -2,11 +2,13 @@
 #include <oda/dspserver.h>
 
 #include <libpd/PdBase.hpp>
+#include <libpd/PdReceiver.hpp>
 #include <libpd/PdTypes.hpp>
 
 #include <algorithm>
 #include <functional>
 #include <iostream>
+#include <memory>
 #include <unordered_set>
 
 namespace oda {
@@ -14,21 +16,33 @@ namespace oda {
 using std::plus;
 using std::string;
 using std::transform;
+using std::unique_ptr;
 using std::unordered_set;
 using std::vector;
 using pd::PdBase;
+using pd::PdReceiver;
 using pd::Patch;
 
 // unnamed namespace
 namespace {
 
+class Receiver : public PdReceiver {
+ public:
+  void print(const string &message) override;
+};
+
 const int             TICK_RATIO = 64;
 
 bool                  started = false;
 PdBase                dsp;
+unique_ptr<Receiver>  receiver;
 unordered_set<Patch*> patches;
 float                 inbuf[6400], outbuf[6400];
 vector<string>        search_paths;
+
+void Receiver::print(const string &message) {
+  std::printf("%s\n", message.c_str());
+}
 
 } // unnamed namespace
 
@@ -39,6 +53,8 @@ Status DSPServer::start() {
     started = true;
     search_paths.clear();
     dsp.computeAudio(true);
+    receiver.reset(new Receiver);
+    dsp.setReceiver(receiver.get());
     std::cout << "DSP tick size: " << tick_size() << std::endl;
     return Status::OK("DSP Server started succesfully");
   }
@@ -93,13 +109,10 @@ void DSPServer::tick(int ticks, vector<float> *signal) {
     // Process global signal
     dsp.processFloat(TICK_RATIO, inbuf, outbuf);
     // Collect individual audio
-    for (Patch *patch : patches) {
-      dsp.readArray(patch->dollarZeroStr() + "-output", temp, tick_size());
-      for (int k = 0; k < tick_size(); ++k)
-        (*signal)[k + i*tick_size()] += temp[k];
-      //transform(signal->begin(), signal->end(),
-      //          temp.begin(), signal->begin() + i*tick_size(), plus<float>());
-    }
+    for (Patch *patch : patches)
+      if (dsp.readArray(patch->dollarZeroStr() + "-output", temp, tick_size()))
+        for (int k = 0; k < tick_size(); ++k)
+          (*signal)[k + i*tick_size()] += temp[k];
   }
 }
 
